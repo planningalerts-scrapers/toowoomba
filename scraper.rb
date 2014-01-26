9a341363ce4e20d976a63d100b4cbab9cc263d10
Daniel O'Connor <daniel.oconnor@gmail.com>
2014-01-26 11:56:04 +1030

    Silence /repo/scraper.rb:4: warning: already initialized constant VERIFY_PEER

--- scraper.rb
+++ scraper.rb
@@ -1,8 +1,10 @@
 require 'scraperwiki'
 require 'openssl'
 #disable encryption validation, we're fetching public data anyway
-OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
-I_KNOW_THAT_OPENSSL_VERIFY_PEER_EQUALS_VERIFY_NONE_IS_WRONG = nil
+silence_warnings do
+  OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+  I_KNOW_THAT_OPENSSL_VERIFY_PEER_EQUALS_VERIFY_NONE_IS_WRONG = nil
+end
 
 require 'mechanize'
 require 'uri'
